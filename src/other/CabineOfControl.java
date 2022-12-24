53b343b16dc20a3d19ba8dda705e0157901e3e6c
notsunshine <alexsupper75@gmail.com>
2022-12-24 15:21:33 +0300

    [MODIFY] deleted getter and setter from CabineOfControl.java

--- src/other/CabineOfControl.java
+++ src/other/CabineOfControl.java
@@ -1,20 +1,11 @@
 package other;
 
 
+import technical.PartsOfRocket;
 import technical.Places;
 
 public class CabineOfControl {
-    public static byte levelOfArmored;
-    public CabineOfControl (byte levelOfArmored){
-        this.levelOfArmored = levelOfArmored;
-    }
-    private static final Places location = Places.CABINE_OF_CONTROL;
 
-    public byte getLevelOfArmored() {
-        return levelOfArmored;
-    }
-
-    public void setLevelOfArmored(byte levelOfArmored) {
-        Rocket.
+    public CabineOfControl(byte levelOfArmored) {
     }
 }
